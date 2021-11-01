68bc5629106c97434e44542eb30330a8a63d6e9b
devasishp305 <debasishp898@gmail.com>
2021-11-01 11:03:14 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+    private int id;
+    private int marks;
+    private int rollNo;
+    private String name;
+
+    public Main(int id, int marks, int rollNo) {
+        this.id = id;
+        this.marks = marks;
+        this.rollNo = rollNo;
+    }
+
+    public void maa() {
+        System.out.println("kddk");
+    }
+/*private int marks;
+private String subject;
+private String details;*/
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
